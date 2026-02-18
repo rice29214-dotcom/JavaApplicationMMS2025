4d0697abd7250d224b8a4be313de2d56318c7fca
PRINCE EDIKAKE <rice29214@gmail.com>
2026-02-18 09:12:12 +0100

    Add files via upload

---
+++ PrimitiveDataType.java
@@ -0,0 +1,36 @@
+public class PrimitiveDataType{
+    public static void main(String[] args){
+		// whole number primitive data type
+		byte age = 30;
+		System.out.printf("I am %d year Old%n",age);
+		
+		short quantityofBags = 10000;
+		System.out.printf("The quantity of bags ordered is %d%n",quantityofBags);
+		
+		
+		int nigeriaPopulation = 2000000;
+		System.out.printf("The Population of Nigeria is %d%n",nigeriaPopulation);
+		
+		long worldsPopulation = 9000000000000000L;
+		System.out.printf("The Worlds Population is %d%n",worldsPopulation);
+		
+		//float-point primitive data type
+          float myBalance = 9999.99990F;
+		  System.out.printf("my Account balance is %.2f%n",myBalance);
+		 
+		 
+		  double cBNBalance = 9874466464.904;
+		  System.out.printf("CBN account balance is %f%n",cBNBalance);
+		  
+		// single  character primitive datatype
+		char symbol = '$';
+		System.out.printf("My Account balance is %c%.2f%n",symbol,myBalance);
+		
+		//boolean primitive data type
+		boolean isjavaFun = true;
+		System.out.printf("Do you love learning java? %b%n",isjavaFun);
+		  
+		  
+		  }
+		  
+}
