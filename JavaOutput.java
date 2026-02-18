f5ffbf318075bd0e1353c647fa9a3958893a3130
rice29214-dotcom <rice29214@gmail.com>
2026-02-18 12:09:54 +0100

    UserInput

---
+++ JavaOutput.java
@@ -0,0 +1,23 @@
+public class JavaOutput{
+
+public static void main(String[] args){
+	System.out.println("Hello World, this is first java program");
+	System.out.println("Learning java is fun");
+	
+	
+	
+	//using the print method
+	System.out.print("Do you love \"Learning Java\"? ");
+	System.out.print("Yes i love Learning Java\n");
+	
+	// using the print f method
+	System.out.printf("My name is Marcus bikelly, I am %d years Old %n",17);
+    }
+
+
+
+
+
+
+}
+	
