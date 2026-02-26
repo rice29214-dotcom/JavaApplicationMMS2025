73b51610cfc7845073e9335915e1de0fc320b8cd
rice29214-dotcom <rice29214@gmail.com>
2026-02-26 11:57:35 +0100

    ForLoop

---
+++ ForLoop2.java
@@ -0,0 +1,9 @@
+public class ForLoop2{
+	public static void main(String[] args){
+		for(int i = 100; i >= 1; i--){
+		   if(i % 2 == 0){
+				System.out.printf("%d is an even number%n",i);
+		   }
+		}   
+	}	
+}		
