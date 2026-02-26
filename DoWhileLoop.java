a116c690c1c36be5091474cb63573be4dade94dc
rice29214-dotcom <rice29214@gmail.com>
2026-02-26 12:21:38 +0100

    ForLoop.java

---
+++ DoWhileLoop.java
@@ -0,0 +1,9 @@
+public class DoWhileLoop{
+	public static void main(String[] args){
+		int i = 18;
+		do{
+			System.out.printf("Greetings to Marcus bikelly %d%n",i);
+			i++;
+		}while(i <= 20);
+	}
+}	
