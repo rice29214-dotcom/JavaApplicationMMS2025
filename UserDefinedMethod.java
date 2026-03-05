51c3fb05d72763ca5ac6687115ae48a646963790
rice29214-dotcom <rice29214@gmail.com>
2026-03-05 10:21:42 +0100

    LocalVariable.java

---
+++ UserDefinedMethod.java
@@ -0,0 +1,20 @@
+public class UserDefinedMethod{
+	public static String userName(String name){
+		return name;
+	}
+	
+	public static int checkAge(int age){
+		if(age >= 18){
+			System.out.println(UserDefinedMethod.userName("John williams ") + "You are an adult");
+		}
+		else{
+			System.out.println(UserDefinedMethod.userName("John williams ") + "You are still a minor");
+			
+		}
+		return age;
+	}
+	
+	public static void main(String[] args){
+		System.out.println(UserDefinedMethod.checkAge(25));
+	}
+}	
---
+++ UsingMathClass.java
@@ -0,0 +1,11 @@
+public class UsingMathClass{
+	public static void main(String[] args){
+		System.out.println("The absolute value is " + Math.abs(-60));
+		System.out.println("The greatest numeber is " + Math.max(25,90));
+		System.out.println("The Smallest numeber is " + Math.min(25,90));
+		System.out.println("The square root of the  numeber is " + Math.sqrt(25));
+		System.out.println("The result of 6 raise to the power of 2 numeber is " + Math.pow(6,2));
+		System.out.println("The random numeber generated is " + Math.random());
+	}
+}	
+	
